fd702ce76a20535c61ab4216ec9a992ad9757bbc
LuizOtavioPolido <polido.luiz@outlook.com>
2023-11-05 22:14:15 -0300

    remoção do input de select language

--- src/modules/WeatherScreen.style.tsx
+++ src/modules/WeatherScreen.style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { ThemesColors } from '../styles/GlobalStyles';
-import { Button, Input } from '@chakra-ui/react';
+import { ThemesColors } from '../styles/GlobalStyles'
+import { Button, Input } from '@chakra-ui/react'
 
 const Container = styled.div`
     height: 100vh;
@@ -13,26 +13,26 @@ const Container = styled.div`
 `
 
 const InputStyled = styled(Input)`
-    height:40px;
-    width:100%;
+    height: 40px;
+    width: 100%;
     font-family: 'OptimusPrinceps', sans-serif;
-    font-size:20px;
+    font-size: 20px;
     font-weight: 500;
-    max-width:50%;
-    margin:15px;
-    border-radius:5px;
-    border:none;
-    outline:none;
-    background-color: ${ThemesColors.whitePrimary   };
+    max-width: 50%;
+    margin: 15px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    background-color: ${ThemesColors.whitePrimary};
 `
 
 const ButtonContainer = styled.div`
-    display:flex;
-    width:'auto';
-    
-    cursor:pointer;
+    display: flex;
+    width: 'auto';
 
-    border-radius: 15px;    
+    cursor: pointer;
+
+    border-radius: 15px;
     :hover {
         background-color: ${ThemesColors.orangePrimaryHover};
     }
@@ -43,59 +43,59 @@ const ButtonContainer = styled.div`
 `
 
 const ButtonStyled = styled(Button)`
-    height:40px;
-    display:flex;
-    width:40px;
-    justify-content:center;
+    height: 40px;
+    display: flex;
+    width: 40px;
+    justify-content: center;
     align-items: center;
     color: #fff;
-    border:none;
-    outline:none;
+    border: none;
+    outline: none;
     padding: 10px;
-    border-radius: 15px;    
-    cursor:pointer;
+    border-radius: 15px;
+    cursor: pointer;
     background-color: ${ThemesColors.orangePrimary};
 `
 
 const InputField = styled.div`
-    display:flex;
-    width:100%;
-    justify-content:center;
+    display: flex;
+    width: 100%;
+    justify-content: center;
     align-items: center;
 `
 
 const TabsContainer = styled.div`
-    display:flex;
-    flex-grow:1;
+    display: flex;
+    flex-grow: 1;
 `
 
 const Tabs = styled.div`
-    height:calc(100% - 150px);
-    width:100%;
+    height: calc(100% - 150px);
+    width: 100%;
     background-color: ${ThemesColors.whitePrimary};
     border-radius: 15px;
     margin: 30px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
     position: relative;
 `
 
 const ErrorOcurredContainer = styled.div`
-    height:50px;
-    width:100%;
+    height: 50px;
+    width: 100%;
     margin: 5px 0px 5px 0px;
-    display:flex;
-    justify-content:center;
-    color:#fff;    
+    display: flex;
+    justify-content: center;
+    color: #fff;
 `
 
 const TabNameLocation = styled.div`
-    display:flex;
-    width:100%;
-    align-items:center;
-    justify-content:center;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
     padding-top: 5px;
 `
 
@@ -104,32 +104,51 @@ const TabDate = styled(TabNameLocation)`
 `
 
 const TabHeader = styled.div`
-    display:flex;
-    justify-content:flex-start;
-    align-items:center;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
     flex-direction: column;
     flex-grow: 1;
-    width:100%;
+    width: 100%;
 `
 
 const TabBody = styled.div`
-    display:flex;
-    width:100%;
+    display: flex;
+    width: 100%;
     flex-grow: 2;
-    justify-content:center;
-    align-items:center;
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
 `
 
 const TabFooter = styled.div`
-    display:flex;
-    width:100%;
+    display: flex;
+    width: 100%;
     flex-grow: 2;
     background-color: #f88;
-    justify-content:center;
-    align-items:center;
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
 `
+
+const SelectContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+`
+
+const Header = styled.div`
+    display: flex;
+    width: 100%;
+`
+
+const TempContainer = styled.div`
+    display:flex;
+    flex-grow: 1;
+    width:100%;
+    flex-direction: row;
+`
+
+
 
 export const WeatherScreenStyle = {
     Container,
@@ -144,5 +163,7 @@ export const WeatherScreenStyle = {
     TabHeader,
     TabBody,
     TabFooter,
-    TabDate
+    TabDate,
+    SelectContainer,
+    Header,
 }
--- src/modules/WeatherScreen.tsx
+++ src/modules/WeatherScreen.tsx
@@ -4,34 +4,53 @@ import { TbDropletSearch } from 'react-icons/tb'
 import axios from 'axios'
 import { Alert } from '@chakra-ui/react'
 import moment from 'moment'
-
+import Select from 'react-select'
+import { languages, placeholderTranslations } from '../utils/constants'
 const API_URL_WITH_KEY =
     'http://api.weatherapi.com/v1/forecast.json?key=ed1b060558fe44599c0234036232910&q='
 
-    
 export default function WeatherScreen() {
     const [value, setValue] = useState('')
     const [dataForecast, setDataForecast] = useState<any>([])
     const [locationForecast, setLocationForecast] = useState<any>()
     const [errorOnRequest, setErrorOnRequest] = useState(false)
-    const [errorMessageRequest, setErrorMessageReques] = useState('')
-    const handleChange = (event: any) => setValue(event.target.value)
+    const [errorMessageRequest, setErrorMessageRequest] = useState('')
+    // const [languageSelected, setLanguageSelected] = useState({
+    //     label: '🇵🇹',
+    //     value: 'pt',
+    // })
 
+    const handleChange = (event: any, state: any) => state(event.target.value)
 
-    
+    // const customStyles = {
+    //     container: (provided: any) => ({
+    //         ...provided,
+    //         width: '90px',
+    //         marginTop: '15px',
+    //         marginLeft: '5px'
+    //     }),
+    // }
+
     function getForecastData(location: string) {
         axios
-            .get(`${API_URL_WITH_KEY}${location}&days=3&aqi=no&alerts=no`)
+            .get(
+                `${API_URL_WITH_KEY}${location}&days=3&aqi=no&alerts=no&lang=pt`
+            )
             .then((response: any) => {
                 const { data } = response
                 setDataForecast(data.forecast)
+                console.log(data)
+
                 setLocationForecast(data.location)
+                setErrorOnRequest(false)
+                setErrorMessageRequest('')
             })
             .catch((response: any) => {
                 setErrorOnRequest(true)
-                setErrorMessageReques(
+                setErrorMessageRequest(
                     `Status code: ${response.response.status} - ${response.message}`
                 )
+                setDataForecast([])
             })
     }
 
@@ -55,24 +74,35 @@ export default function WeatherScreen() {
             getForecastData(value)
         }
     }
-    
+
     return (
         <WeatherScreenStyle.Container>
-            <WeatherScreenStyle.InputField>
-                <WeatherScreenStyle.InputStyled
-                    value={value}
-                    onChange={handleChange}
-                    placeholder="Digite uma cidade"
-                    onKeyUp={(e: any) => handleKeyUp(e)}
-                />
-                <WeatherScreenStyle.ButtonContainer
-                    onClick={() => getForecastData(value)}
-                >
-                    <WeatherScreenStyle.ButtonStyled>
-                        <TbDropletSearch size={20} />
-                    </WeatherScreenStyle.ButtonStyled>
-                </WeatherScreenStyle.ButtonContainer>
-            </WeatherScreenStyle.InputField>
+            <WeatherScreenStyle.Header>
+                {/* <WeatherScreenStyle.SelectContainer>
+                    <Select
+                        value={languageSelected}
+                        onChange={(obj: any) => setLanguageSelected(obj)}
+                        options={languages}
+                        styles={customStyles}
+                    />
+                </WeatherScreenStyle.SelectContainer> */}
+                <WeatherScreenStyle.InputField>
+                    <WeatherScreenStyle.InputStyled
+                        value={value}
+                        onChange={(e: any) => handleChange(e, setValue)}
+                        placeholder="Digite uma cidade"
+                        onKeyUp={(e: any) => handleKeyUp(e)}
+                    />
+                    <WeatherScreenStyle.ButtonContainer
+                        onClick={() => getForecastData(value)}
+                    >
+                        <WeatherScreenStyle.ButtonStyled>
+                            <TbDropletSearch size={20} />
+                        </WeatherScreenStyle.ButtonStyled>
+                    </WeatherScreenStyle.ButtonContainer>
+                </WeatherScreenStyle.InputField>
+            </WeatherScreenStyle.Header>
+
             {renderErrorIfOcurred()}
             <WeatherScreenStyle.TabsContainer>
                 {dataForecast?.forecastday?.map((item: any, index: number) => {
@@ -92,7 +122,7 @@ export default function WeatherScreen() {
                                 {item?.day?.condition?.text}
                             </WeatherScreenStyle.TabBody>
                             <WeatherScreenStyle.TabFooter>
-                                teste
+                                
                             </WeatherScreenStyle.TabFooter>
                         </WeatherScreenStyle.Tabs>
                     )
